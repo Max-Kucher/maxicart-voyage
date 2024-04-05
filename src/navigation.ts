d9ca0690d6b7b9dfb07da1ee956036e852e4d7f8
Max-Kucher <mkuchirov@gmail.com>
2024-04-05 21:44:41 +0300

    Fixes for i18n

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,7 @@
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
 
 export const locales = ['en', 'ru'] as const;
-export const localePrefix = 'as-needed';
+export const localePrefix = 'always';
 
-export const {Link, redirect, usePathname, useRouter} =
+export const {Link, redirect, usePathname, useRouter, permanentRedirect} =
     createSharedPathnamesNavigation({locales, localePrefix});
