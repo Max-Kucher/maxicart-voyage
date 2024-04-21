0ad8cc9b8be498aad404e35aaa55af06818aee06
Max-Kucher <mkuchirov@gmail.com>
2024-04-21 21:52:29 +0300

    Check env in prod

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,6 +2,8 @@
 export const isProd: boolean = process?.env.NODE_ENV === 'production';
 export const BackendBaseUrl: string = process?.env?.NEXT_PUBLIC_BACKEND_BASE_URL?.length ? process?.env?.NEXT_PUBLIC_BACKEND_BASE_URL : 'http://80.89.230.106:81';
 
+console.log(process?.env);
+
 const appConfig = {
     supportedLanguages: {
         ru: {},
